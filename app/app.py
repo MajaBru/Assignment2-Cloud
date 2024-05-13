1cd3e19d879aaaa446ed210a6f85d1c583d6957b
Maja <122542734+MajaBru@users.noreply.github.com>
2024-05-13 18:23:48 +0200

    .

--- app/app.py
+++ app/app.py
@@ -32,4 +32,4 @@ def index():
 
 # If ran (not just imported from elsewhere), launch the server.
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port:5000)
+    app.run(host="0.0.0.0", port=5000)
