030d9d0a5d7849a58449ea37f9e8f93e685271b0
Ekkeskit <126162842+Ekkeskit@users.noreply.github.com>
2024-05-19 05:52:42 +0200

    Testss

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 import mysql.connector
 
-app = Flask(__name__, template_folder='../front-end')
+app = Flask(__name__, template_folder='front-end')
 
 
 # endra på host db
