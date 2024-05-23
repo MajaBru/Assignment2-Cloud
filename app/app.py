86cff71aec0491ca701be2b8d8389bf29dd4bcd1
Ekkeskit <126162842+Ekkeskit@users.noreply.github.com>
2024-05-23 21:04:12 +0200

    more post setup

--- app/app.py
+++ app/app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
-import MySQLdb.cursors
+from flask import Flask, render_template, request, redirect, session, jsonify
 import re, os
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pymysql
-from models import db, User, Post, Like
 from flask_bcrypt import Bcrypt
+from models import db, User, Post, Like
 
 app = Flask(__name__, template_folder='../front-end', static_folder='../front-end/static')
 
 app.secret_key = 'your_secret_key_here'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/redditdb'
+# Updated to remove the password
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/redditdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SESSION_FILE_DIR'] = os.path.join(app.root_path, 'sessions')
@@ -20,8 +20,17 @@ os.makedirs(app.config['SESSION_FILE_DIR'], exist_ok=True)
 # Initialize
 db.init_app(app)
 bcrypt = Bcrypt(app)
-connection = pymysql.connect(host='localhost', user='root', password='root', db='redditdb')
 
+# Make sure to create the database if it doesn't exist
+def create_database():
+    connection = pymysql.connect(host='localhost', user='root')
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS redditdb")
+    cursor.execute("SHOW DATABASES")
+    for database in cursor:
+        print(database)
+    cursor.close()
+    connection.close()
 
 @app.route('/')
 def index():
@@ -35,7 +44,7 @@ def register():
         email = request.form['email']
         password = request.form['password']
 
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8') #hashing with bcrypt
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')  # Hashing with bcrypt
 
         existing_user = User.query.filter_by(username=username).first()
 
@@ -55,6 +64,7 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
@@ -62,9 +72,7 @@ def login():
         user = User.query.filter_by(username=username).first()
 
         if user and bcrypt.check_password_hash(user.password, password):
-            # setting user session
             session['loggedin'] = True
-
             session['username'] = user.username
             session['email'] = user.email
             return redirect('/home')
@@ -74,14 +82,12 @@ def login():
 
     return render_template('login.html')
 
-
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
     session.pop('username', None)
     session.pop('email', None)
     return redirect('/login')
-
 
 @app.route('/home', methods=['GET'])
 def home():
@@ -90,34 +96,14 @@ def home():
         email = session.get('email')
         if username and email:
             return render_template('home.html', username=session['username'])
-    
     return redirect('/login')
 
-
-
-
-
-# retreive all users json format
+# Retrieve all users in JSON format
 @app.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
     user_list = [{'id': user.id, 'username': user.username, 'email': user.email, 'created_at': user.created_at} for user in users]
     return jsonify(user_list)
-
-
-# make db if it doesnt exist
-def create_database():
-    connection = pymysql.connect(host='localhost', user='root', password='root')
-    cursor = connection.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS redditdb")
-    cursor.execute("SHOW DATABASES")
-
-    for databases in cursor:
-        print(databases)
-
-    cursor.close()
-    connection.close()
-
 
 @app.route('/posts', methods=['POST'])
 def create_post():
@@ -137,6 +123,9 @@ def like_post():
     db.session.commit()
     return jsonify({'message': 'Post liked!'})
 
+@app.route('/create_post.html')
+def create_post_page():
+    return render_template('create_post.html')
 
 @app.route('/posts', methods=['GET'])
 def get_posts():
@@ -155,7 +144,6 @@ def create_tables():
     with app.app_context():
         db.create_all()
         print('Tables created')
-
 
 if __name__ == '__main__':
     create_database()
