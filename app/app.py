db7d256c3aa084838feed70931b928bb3153d2e4
Ekkeskit <126162842+Ekkeskit@users.noreply.github.com>
2024-05-26 23:37:56 +0200

    adjustments for testing

--- app/app.py
+++ app/app.py
@@ -265,4 +265,4 @@ def create_tables():
         print('Tables created')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
